23cac8fb5f089150508237a081ea0101902632fe
E. Sam Berney <esberney@gmail.com>
2019-02-18 20:41:21 -0800

    Added Carter server

---
+++ Server/HomeModule.cs
@@ -0,0 +1,20 @@
+﻿namespace Server
+{
+    using Carter;
+    using Microsoft.AspNetCore.Http;
+
+    public class HomeModule : CarterModule
+    {
+        public HomeModule()
+        {
+            Get("/", async(req, res, routeData) => await res.WriteAsync("Hello from Carter!"));
+
+            Get("/validate/{word}", async(req, res, routeData) => {
+              var word = routeData.word;
+              res.AsJson(new {
+                  myWord = word
+              });
+            });
+        }
+    }
+}
