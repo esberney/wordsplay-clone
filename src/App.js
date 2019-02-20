b6ff71ce00fc85c2a5389d5860935fb49666a8af
sberney <sam@samuelberney.com>
2019-02-19 22:44:36 -0800

    Muuri demo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import { ListGroup } from 'react-bootstrap';
 import { connect, Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+import MuuriGrid from 'react-muuri';
+import './MuuriGrid.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,6 +15,66 @@ import { TextEntry } from './TextEntry.js';
 import { MyWords } from './MyWords.js'
 import { Wordlist } from './Wordlist.js';
 import { isWordAsync } from './is-word-async.js';
+
+
+class MyThing extends Component {
+  constructor () {
+    super();
+
+    this.removeElement = this.removeElement.bind(this);
+  }
+
+  componentDidMount () {
+    this.grid = new MuuriGrid({
+      node: this.gridElement,
+      defaultOptions: {
+        dragEnabled: true // See Muuri's documentation for other option overrides.
+      },
+    });
+
+    // An example of how to use `getEvent()` to make `synchronize()` update the grid.
+    this.grid.getEvent('dragEnd');
+  }
+
+  componentWillUnmount () {
+    this.grid.getMethod('destroy'); // Required: Destroy the grid when the component is unmounted.
+  }
+
+  removeElement () {
+    // An example of how to use `getMethod()` to remove an element from the grid.
+    if (this.gridElement && this.gridElement.children.length) {
+      this.grid.getMethod('remove', this.gridElement.children[0], {removeElements: true});
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        {/* Assign a ref to the grid container so the virtual DOM will ignore it for now (WIP). */}
+        <div ref={gridElement => this.gridElement = gridElement}>
+          {/* Required: `item` and `item-content` classNames */}
+          <div className="item box1">
+            <div className="item-content">
+              Box 1
+            </div>
+          </div>
+          <div className="item box2">
+            <div className="item-content">
+              Box 2
+            </div>
+          </div>
+        </div>
+        <button
+          className="button"
+          onClick={() => this.removeElement()}
+        >
+          Remove 1st Element
+        </button>
+      </div>
+    )
+  }
+}
+
 
 const myLetters = [
   ['A', 'B', 'C', 'H'],
@@ -80,6 +142,7 @@ class App extends Component {
 
     return (
       <div>
+        <MyThing />
         <FlexCentered>
           <Grid letters2d={myLetters} highlights={highlights} />
         </FlexCentered>
